6af4db12e53881a7b7638076acb6f7f090d7a0a4
Vansh16aug <kumarvansh16aug@gmail.com>
2025-04-21 20:09:10 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://cal-connect.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
